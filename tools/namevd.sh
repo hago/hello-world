9ee53504ea042aa1fb46eb1fc4b494f5a4776ed5
Chaojun Sun <suncjs@hagoapp.com>
2024-04-10 15:45:26 +0800

    create a script to rename video and image files under a directory, e.g. a.mp4, b.jpg under directory cc will be renamed to cc/cc.mp4 and cc/cc.jpg

---
+++ tools/namevd.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "usage $0 [dir]"
+	exit 2
+fi
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+IFS=$'\n'
+shopt -s nocasematch
+while (( "$#" )); do
+	p=${1%/}
+	#echo "p: $p"
+	for f in `ls $1`
+	do
+		ext=''
+		#echo "f: $f"
+		if [[ $f == *.hevc.mp4 ]]; then
+			ext=".hevc.mp4"
+		elif [[ $f == *.mp4 ]]; then
+			ext=".mp4"
+		elif [[ $f == *.mkv ]]; then
+			ext=".mkv"
+		elif [[ $f == *.thumb.jpg ]]; then
+			ext=".thumb.jpg"
+		elif [[ $f == *.jpg ]]; then
+			ext=".jpg"
+		elif [[ $f == *.thumb.png ]]; then
+			ext=".thumb.png"
+		elif [[ $f == *.png ]]; then
+			ext=".png"
+		else
+			echo -e "${RED}skip: $f" >&2
+		fi
+		if [[ "$ext" != "" ]]; then
+			t="$p/$p$ext"
+			#echo "t: $t"
+			if [ -f "$t" ]; then
+				echo -e "${RED}EXISTED and skip: $t" >&2
+			else
+				echo -e "${GREEN}mv \"$p/$f\" \"$p/$p$ext\""
+			fi
+		fi
+	done
+	shift
+done
