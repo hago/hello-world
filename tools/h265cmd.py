608e90e282bf3ffef747cdeeba54d2ac395a8bdd
Chaojun Sun <suncjs@hagoapp.com>
2024-05-15 10:30:52 +0800

    support file enc and generating windows batch file

--- tools/h265cmd.py
+++ tools/h265cmd.py
@@ -17,6 +17,11 @@ from pyffprobe import probe, codec
 
 VIDEO_FILE_TYPES = ['.mkv', '.mp4', 'avi', '.rmvb']
 
+class command:
+    def __init__(self, comment, cmd):
+        self.comment = comment
+        self.cmd = cmd
+
 class pathrunner():
     def __init__(self, arg) -> None:
         self.cmds = []
@@ -25,6 +30,8 @@ class pathrunner():
         self.root = os.path.realpath(arg.directory)
         self.h264subregexes = [re.compile(re.escape(s), re.I) for s in ["h264", "x264", "avc"]]
         self.skiplist = [] if arg.skip==None else [os.path.realpath(x) for x in arg.skip]
+        self.bash = not arg.win
+        self.enc = arg.encoding
         if not os.path.exists(self.root):
             raise FileExistsError('%s not existed or not accessible' % self.root)
 
@@ -55,10 +62,9 @@ class pathrunner():
                 continue
             br265 = self.__calch265btr(st.codec, int(st.codec.bitrate))
             cmd = '''ffmpeg -i "%s%s" -map 0 -c:v hevc -b:v %dk -c:a copy -c:s copy "%s.hevc.mp4"''' % (fn, ext, br265, self.__targetname(fn))
+            comment = "File is encoded by %s with %f" % (st.codec.name, st.codec.bitrate)
             print(cmd)
-            self.cmds.append("#File is encoded by %s with %f" % (st.codec.name, st.codec.bitrate))
-            self.cmds.append(cmd)
-            self.cmds.append("sleep 10")
+            self.cmds.append(command(comment, cmd))
             break
 
     def __targetname(self, filename: str):
@@ -90,19 +96,42 @@ class pathrunner():
         self.__writesh()
 
     def __writesh(self):
+        if self.bash:
+            self.__writebash()
+        else:
+            self.__writecmd()
+
+    def __writebash(self):
+        sep = '\n'.encode(self.enc)
         with open("h265.sh", "wb") as fp:
             fp.write(b"#!/bin/sh\n\n")
             for cmd in self.cmds:
-                fp.write(cmd.encode('utf8'))
-                fp.write(b'\n')
+                fp.write(('#%s' % cmd.comment).encode(self.enc))
+                fp.write(sep)
+                fp.write(cmd.cmd.encode(self.enc))
+                fp.write(sep)
+                fp.write('sleep 10'.encode(self.enc))
+                fp.write(sep)
+        pass
+
+    def __writecmd(self):
+        sep = '\r\n'.encode(self.enc)
+        with open("h265.cmd", "wb") as fp:
+            for cmd in self.cmds:
+                fp.write(('REM %s' % cmd.comment).encode(self.enc))
+                fp.write(sep)
+                fp.write(cmd.cmd.encode(self.enc))
+                fp.write(sep)
         pass
 
 def buildargparser():
-    parser = argparse.ArgumentParser(description='Generate ffmpeg commands')
+    parser = argparse.ArgumentParser(description='Generate a script which run ffmpeg to encode video files in a directory with hevc.')
     parser.add_argument('directory', help = 'the directory to search in')
     parser.add_argument('-h264br', '--h264-bitrate-ratio', help = 'target bit rate ratio for original H.264 video', default=2/3, type=float)
     parser.add_argument('-br', '--default-bitrate-ratio', help = 'target bit rate ratio for any other original video codecs', default=0.5, type=float)
     parser.add_argument('-s', '--skip', help = 'skip files', nargs='*')
+    parser.add_argument('-w', '--win', help = 'output script in windows bacth', action='store_true', default=False)
+    parser.add_argument('-enc', '--encoding', help = 'encoding of the output file', default='utf-8')
     return parser
 
 if __name__=='__main__':
