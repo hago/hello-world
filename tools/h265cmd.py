fd9b2855c0dd8437cdd0e9072c8fead0827143be
suncjs <suncjs@hagoapp.com>
2024-11-07 16:55:00 +0800

    output format adjust

--- tools/h265cmd.py
+++ tools/h265cmd.py
@@ -147,10 +147,11 @@ class pathrunner():
                 for comment in cmd.comments:
                     fp.write(('#%s' % comment).encode(self.enc))
                     fp.write(sep)
-                fp.write(sep)
                 fp.write(cmd.cmd.encode(self.enc))
                 fp.write(sep)
+                fp.write(sep)
                 fp.write('sleep 10'.encode(self.enc))
+                fp.write(sep)
                 fp.write(sep)
         pass
 
